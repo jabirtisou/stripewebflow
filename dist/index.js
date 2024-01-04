2d70bcdeaced1c8543182d98d8d82455885b8073
Jabir Tisoudali <44145503+jabirtisou@users.noreply.github.com>
2024-01-04 18:54:05 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -20,23 +20,8 @@
     if (!idealStripeElement)
       return;
 
-    const appearance = {
-      theme: 'stripe',
-    
-      variables: {
-        colorPrimary: '#0570de',
-        colorBackground: '#ffffff',
-        colorText: '#30313d',
-        colorDanger: '#df1b41',
-        fontFamily: 'Ideal Sans, system-ui, sans-serif',
-        spacingUnit: '2px',
-        borderRadius: '4px',
-      }
-    };
-    
-    // Pass the appearance object to the Elements instance
-    const elements = stripe.elements({clientSecret, appearance});
 
+    const elements = stripe.elements({clientSecret});
     const idealBank = elements.create('idealBank');
     idealBank.mount(idealStripeElement);
     const card = elements.create('card');
